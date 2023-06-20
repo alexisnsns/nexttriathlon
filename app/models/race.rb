42fe316a19ad4f446c09545e617ca3cd5762807a
alexis nurdin <alexisns@proton.me>
2023-06-20 18:19:54 +0200

    css and fix search

--- app/models/race.rb
+++ app/models/race.rb
@@ -2,8 +2,7 @@ class Race < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
 
-  validates :title, uniqueness: true
-  validates :description, uniqueness: true
+  validates :title, :description, uniqueness: true
   validates :date, :bike, :swim, :run, :format, :organizer, :photo, :address, presence: true
   has_one_attached :photo
 
@@ -11,6 +10,6 @@ class Race < ApplicationRecord
   after_validation :geocode, if: :will_save_change_to_address?
 
   def self.search(search)
-    where('title ILIKE ? OR format ILIKE ?', "%#{search}%", "%#{search}%")
+    where('title ILIKE ?', "%#{search}%")
   end
 end
